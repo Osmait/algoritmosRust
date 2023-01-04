91ccee14d5596c2b1687d00b7b4f52176fe08d30
Saul Burgos <saulburgos6@gmail.com>
2023-01-03 22:18:50 -0400

    sorting
    Signed-off-by: Saul Burgos <saulburgos6@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::vec;
 
+mod searching;
 mod sorting;
 fn main() {
     let mut ve1 = vec![6, 5, 3, 2, 8, 1];
-    let lista_str = ["hola", "prueba"];
+
+    let mut lista_str = vec![20, 40, 10, 15, 60];
+    // sorting::bucket_sort::bucket_sort(&mut lista_str);
     sorting::buble_sort::bubble_sort(&mut ve1);
+    sorting::quick_sort(&mut lista_str);
     println!("{ve1:?}",);
     println!("{lista_str:?}",);
 }
--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -1,1 +1,8 @@
 pub mod buble_sort;
+pub mod bucket_sort;
+mod insertion_sort;
+mod quick_sort;
+
+pub use self::insertion_sort::insertion_sort;
+pub use self::quick_sort::partition;
+pub use self::quick_sort::quick_sort;
